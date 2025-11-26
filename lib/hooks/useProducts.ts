aed34215153051834e1b0d0d99f0be6494d86106
deploy <newgis@yandex.ru>
2025-11-26 09:46:44 +0000

    fix: useProducts.ts

--- lib/hooks/useProducts.ts
+++ lib/hooks/useProducts.ts
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query"
 import { apiClient } from "@/lib/api/client"
-import { Product, ProductsParams } from "@/lib/types/product"
+import { Product, GetProductsParams } from "@/lib/types/product"
 import { ListResponse } from "@/lib/types/api"
 
-export const useProducts = (params: ProductsParams = { page: 1, size: 20, sort_order: "desc"}) => {
+export const useProducts = (params: GetProductsParams = { page: 1, size: 20, sort_order: "desc"}) => {
     return useQuery<ListResponse<Product[]>>({
         queryKey: ["products", params],
         queryFn: async () => {
